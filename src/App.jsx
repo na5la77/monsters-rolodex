7a89269e1147ec5e8a265f33b74558fcbfb3785e
Shadi Nakhla <shadinakhla13@gmail.com>
2022-11-19 09:52:22 +0200

    implemented search functionality on sudo api call

---
+++ src/App.jsx
@@ -0,0 +1,61 @@
+import { Component } from "react";
+import "./App.css";
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+      searchField:''
+    };
+    console.log('constructor')
+  }
+
+  componentDidMount() {
+    console.log('mount')
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          }
+        )
+      );
+  }
+
+  render() {
+    console.log('render')
+
+    const filteredMonsters = this.state.monsters.filter((monster)=>{
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField)
+
+    });
+    return (
+      <div className="App">
+      <input className='search-box' type='search' placeholder="Search" onChange={
+        (event) => {
+          const searchField = event.target.value.toLocaleLowerCase();
+         
+          this.setState(() => {
+            return {searchField}
+          })
+
+        }
+      }/>
+        {filteredMonsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
+  }
+}
+
+export default App;
